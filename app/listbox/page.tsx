4666c4f25f7c431ea2ac6e9cb8dd315e5a4ea8f7
Changnam Go <changnam.go@gmail.com>
2025-02-28 08:12:40 +0900

    headlessui listbox , name 과 value 를 주면 hidden input 에 적용됨. object 로 바인딩 되어 assignee%5Bid%5D=1&assignee%5Bname%5D=Durward+Reynolds 로 날아감

--- app/listbox/page.tsx
+++ app/listbox/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import {
+  Field,
+  Label,
   Listbox,
   ListboxButton,
   ListboxOption,
@@ -9,31 +11,40 @@ import {
 import { useState } from "react";
 
 const people = [
-  { id: 1, name: "Durward Reynolds", available: true },
-  { id: 2, name: "Kenton Towne", available: true },
-  { id: 3, name: "Therese Wunsch", available: true },
-  { id: 4, name: "Benedict Kessler", available: false },
-  { id: 5, name: "Katelyn Rohan", available: true },
+  { id: 1, name: "Durward Reynolds" },
+  { id: 2, name: "Kenton Towne" },
+  { id: 3, name: "Therese Wunsch" },
+  { id: 4, name: "Benedict Kessler" },
+  { id: 5, name: "Katelyn Rohan" },
 ];
 
 export default function Example() {
   const [selectedPerson, setSelectedPerson] = useState(people[0]);
 
   return (
-    <Listbox value={selectedPerson} onChange={setSelectedPerson}>
-      <ListboxButton>{selectedPerson.name}</ListboxButton>
-      <ListboxOptions anchor="bottom">
-        {people.map((person) => (
-          <ListboxOption
-            key={person.id}
-            value={person}
-            disabled={!person.available}
-            className="data-[focus]:bg-blue-100 data-[disabled]:opacity-50"
-          >
-            {person.name}
-          </ListboxOption>
-        ))}
-      </ListboxOptions>
-    </Listbox>
+    <form action="http://localhost:8080/api/posts/1/like" method="post">
+      <Field>
+        <Label>Assignee:</Label>
+        <Listbox
+          name="assignee"
+          value={selectedPerson}
+          onChange={setSelectedPerson}
+        >
+          <ListboxButton>{selectedPerson.name}</ListboxButton>
+          <ListboxOptions anchor="bottom">
+            {people.map((person) => (
+              <ListboxOption
+                key={person.id}
+                value={person}
+                className="data-[focus]:bg-blue-100"
+              >
+                {person.name}
+              </ListboxOption>
+            ))}
+          </ListboxOptions>
+        </Listbox>
+      </Field>
+      <button>Submit</button>
+    </form>
   );
 }
