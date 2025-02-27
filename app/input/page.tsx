8e48316ed7f1db842c8e1b324cccfcc6b34006bf
Changnam Go <changnam.go@gmail.com>
2025-02-28 07:53:34 +0900

    headlessui input field 를 disabled 로 하고 data-disable 로 스타일링

--- app/input/page.tsx
+++ app/input/page.tsx
@@ -2,12 +2,12 @@ import { Description, Field, Input, Label } from "@headlessui/react";
 
 export default function Example() {
   return (
-    <Field>
-      <Label>Name</Label>
-      <Description>
+    <Field disabled>
+      <Label className="data-[disabled]:opacity-50">Name</Label>
+      <Description className="data-[disabled]:opacity-50">
         Use your real name so people will recognize you.
       </Description>
-      <Input name="full_name" />
+      <Input name="full_name" className="data-[disabled]:bg-gray-100" />
     </Field>
   );
 }
