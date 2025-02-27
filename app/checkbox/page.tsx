a68161a67050bd3b227dc4f0469364ded5dd8035
Changnam Go <changnam.go@gmail.com>
2025-02-27 20:20:51 +0900

    headlessui checkbox , render prop 을 활용하여 스타일링, Button 과 달리 Checkbox 는 함수를 넘기는게 가능함. Button 은 내부적으로 client component로 정의되어 있는것 같음

--- app/checkbox/page.tsx
+++ app/checkbox/page.tsx
@@ -1,30 +1,41 @@
 "use client";
 
 import { Checkbox } from "@headlessui/react";
-import { useState } from "react";
+import clsx from "clsx";
+import { Fragment, useState } from "react";
 
 export default function Example() {
   const [enabled, setEnabled] = useState(true);
 
   return (
-    <Checkbox
-      checked={enabled}
-      onChange={setEnabled}
-      className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
-    >
-      {/* Checkmark icon */}
-      <svg
-        className="stroke-white opacity-0 group-data-[checked]:opacity-100"
-        viewBox="0 0 14 14"
-        fill="none"
-      >
-        <path
-          d="M3 8L6 11L11 3.5"
-          strokeWidth={2}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+    <Checkbox checked={enabled} onChange={setEnabled} as={Fragment}>
+      {({ checked, disabled }) => (
+        <span
+          className={clsx(
+            "block size-4 rounded border",
+            !checked && "bg-white",
+            checked && !disabled && "bg-blue-500",
+            checked && disabled && "bg-gray-500",
+            disabled && "cursor-not-allowed opacity-50"
+          )}
+        >
+          <svg
+            className={clsx(
+              "stroke-white",
+              checked ? "opacity-100" : "opacity-0"
+            )}
+            viewBox="0 0 14 14"
+            fill="none"
+          >
+            <path
+              d="M3 8L6 11L11 3.5"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </span>
+      )}
     </Checkbox>
   );
 }
