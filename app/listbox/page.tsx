e24a588aef3611fa9e47b5109b2ea91ec218b667
Changnam Go <changnam.go@gmail.com>
2025-02-28 08:08:29 +0900

    headlessui listbox , 항목에 disabled 주고 data-[disabled] 로 스타일링

--- app/listbox/page.tsx
+++ app/listbox/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import {
-  Description,
-  Field,
-  Label,
   Listbox,
   ListboxButton,
   ListboxOption,
@@ -12,36 +9,31 @@ import {
 import { useState } from "react";
 
 const people = [
-  { id: 1, name: "Durward Reynolds" },
-  { id: 2, name: "Kenton Towne" },
-  { id: 3, name: "Therese Wunsch" },
-  { id: 4, name: "Benedict Kessler" },
-  { id: 5, name: "Katelyn Rohan" },
+  { id: 1, name: "Durward Reynolds", available: true },
+  { id: 2, name: "Kenton Towne", available: true },
+  { id: 3, name: "Therese Wunsch", available: true },
+  { id: 4, name: "Benedict Kessler", available: false },
+  { id: 5, name: "Katelyn Rohan", available: true },
 ];
 
 export default function Example() {
   const [selectedPerson, setSelectedPerson] = useState(people[0]);
 
   return (
-    <Field disabled>
-      <Label>Assignee:</Label>
-      <Description>
-        This person will have full access to this project.
-      </Description>
-      <Listbox value={selectedPerson} onChange={setSelectedPerson}>
-        <ListboxButton>{selectedPerson.name}</ListboxButton>
-        <ListboxOptions anchor="bottom">
-          {people.map((person) => (
-            <ListboxOption
-              key={person.id}
-              value={person}
-              className="data-[focus]:bg-blue-100"
-            >
-              {person.name}
-            </ListboxOption>
-          ))}
-        </ListboxOptions>
-      </Listbox>
-    </Field>
+    <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+      <ListboxButton>{selectedPerson.name}</ListboxButton>
+      <ListboxOptions anchor="bottom">
+        {people.map((person) => (
+          <ListboxOption
+            key={person.id}
+            value={person}
+            disabled={!person.available}
+            className="data-[focus]:bg-blue-100 data-[disabled]:opacity-50"
+          >
+            {person.name}
+          </ListboxOption>
+        ))}
+      </ListboxOptions>
+    </Listbox>
   );
 }
