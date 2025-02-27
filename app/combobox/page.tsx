6be091526df0817f04f2f4a128fb39a5ac1f9b2c
Changnam Go <changnam.go@gmail.com>
2025-02-27 23:33:05 +0900

    headlessui combox multiple 선택 가능하도록 함. value 에는 array 를 넘김

--- app/combobox/page.tsx
+++ app/combobox/page.tsx
@@ -1,54 +1,45 @@
 "use client";
 
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxOption,
-  ComboboxOptions,
-} from "@headlessui/react";
-import { useState } from "react";
+import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/react'
+import { useState } from 'react'
 
 const people = [
-  "Durward Reynolds",
-  "Kenton Towne",
-  "Therese Wunsch",
-  "Benedict Kessler",
-  "Katelyn Rohan",
-];
+  { id: 1, name: 'Durward Reynolds' },
+  { id: 2, name: 'Kenton Towne' },
+  { id: 3, name: 'Therese Wunsch' },
+  { id: 4, name: 'Benedict Kessler' },
+  { id: 5, name: 'Katelyn Rohan' },
+]
 
 export default function Example() {
-  const [selectedPerson, setSelectedPerson] = useState(people[0]);
-  const [query, setQuery] = useState("");
+  const [selectedPeople, setSelectedPeople] = useState([people[0], people[1]])
+  const [query, setQuery] = useState('')
 
   const filteredPeople =
-    query === ""
+    query === ''
       ? people
       : people.filter((person) => {
-          return person.toLowerCase().includes(query.toLowerCase());
-        });
+          return person.name.toLowerCase().includes(query.toLowerCase())
+        })
 
   return (
-    <Combobox
-      value={selectedPerson}
-      onChange={setSelectedPerson}
-      onClose={() => setQuery("")}
-    >
-      <ComboboxInput
-        aria-label="Assignee"
-        onChange={(event) => setQuery(event.target.value)}
-        className="border"
-      />
+    <Combobox multiple value={selectedPeople} onChange={setSelectedPeople} onClose={() => setQuery('')}>
+      {selectedPeople.length > 0 && (
+        <ul>
+          {selectedPeople.map((person) => (
+            <li key={person.id}>{person.name}</li>
+          ))}
+        </ul>
+      )}
+      <ComboboxInput aria-label="Assignees" onChange={(event) => setQuery(event.target.value)} 
+        className="border" />
       <ComboboxOptions anchor="bottom" className="border empty:invisible">
         {filteredPeople.map((person) => (
-          <ComboboxOption
-            key={person}
-            value={person}
-            className="data-[focus]:bg-blue-100"
-          >
-            {person}
+          <ComboboxOption key={person.id} value={person} className="data-[focus]:bg-blue-100">
+            {person.name}
           </ComboboxOption>
         ))}
       </ComboboxOptions>
     </Combobox>
-  );
+  )
 }
