2ec4369e657a89b8f60112155751e85ba34c861f
Changnam Go <changnam.go@gmail.com>
2025-02-27 20:43:54 +0900

    headlessui checkbox Field 에 name 을 주면 hidden 으로 input 필드에 값이 저장되어 서버로 날아간다. terms-of-service=on 값으로 날아감

--- app/checkbox/page.tsx
+++ app/checkbox/page.tsx
@@ -1,21 +1,18 @@
 "use client";
 
-import { Checkbox, Description, Field, Label } from "@headlessui/react";
+import { Checkbox } from "@headlessui/react";
 import { useState } from "react";
 
 export default function Example() {
   const [enabled, setEnabled] = useState(false);
 
   return (
-    <Field disabled>
-      <Label className="data-[disabled]:opacity-50">Enable beta features</Label>
-      <Description className="data-[disabled]:opacity-50">
-        This will give you early access to new features we're developing.
-      </Description>
+    <form action="http://localhost:8080/api/posts/1/like" method="post">
       <Checkbox
         checked={enabled}
         onChange={setEnabled}
-        className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500 data-[disabled]:cursor-not-allowed data-[disabled]:opacity-50 data-[checked]:data-[disabled]:bg-gray-500"
+        name="terms-of-service"
+        className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
       >
         <svg
           className="stroke-white opacity-0 group-data-[checked]:opacity-100"
@@ -30,6 +27,7 @@ export default function Example() {
           />
         </svg>
       </Checkbox>
-    </Field>
+      <button>Submit</button>
+    </form>
   );
 }
