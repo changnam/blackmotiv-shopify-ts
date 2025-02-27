5065809325892ea6490cea9bc023efc8e7ae3258
Changnam Go <changnam.go@gmail.com>
2025-02-27 21:46:43 +0900

    headlessui combobox inputbox 에 filter 적용

--- app/combobox/page.tsx
+++ app/combobox/page.tsx
@@ -2,26 +2,23 @@
 
 import {
   Combobox,
-  ComboboxButton,
   ComboboxInput,
   ComboboxOption,
   ComboboxOptions,
 } from "@headlessui/react";
-import { CheckIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
-import clsx from "clsx";
 import { useState } from "react";
 
 const people = [
-  { id: 1, name: "Tom Cook" },
-  { id: 2, name: "Wade Cooper" },
-  { id: 3, name: "Tanya Fox" },
-  { id: 4, name: "Arlene Mccoy" },
-  { id: 5, name: "Devon Webb" },
+  { id: 1, name: "Durward Reynolds" },
+  { id: 2, name: "Kenton Towne" },
+  { id: 3, name: "Therese Wunsch" },
+  { id: 4, name: "Benedict Kessler" },
+  { id: 5, name: "Katelyn Rohan" },
 ];
 
 export default function Example() {
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
   const [query, setQuery] = useState("");
-  const [selected, setSelected] = useState(people[1]);
 
   const filteredPeople =
     query === ""
@@ -31,46 +28,27 @@ export default function Example() {
         });
 
   return (
-    <div className="mx-auto h-screen w-52 pt-20 border border-red-500">
-      <Combobox
-        value={selected}
-        onChange={(value) => setSelected(value)}
-        onClose={() => setQuery("")}
-      >
-        <div className="relative">
-          <ComboboxInput
-            className={clsx(
-              "w-full rounded-lg border-none bg-black/5 py-1.5 pr-8 pl-3 text-sm/6 text-black",
-              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25"
-            )}
-            displayValue={(person) => person?.name}
-            onChange={(event) => setQuery(event.target.value)}
-          />
-          <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
-            <ChevronDownIcon className="size-4 fill-black/60 group-data-[hover]:fill-black" />
-          </ComboboxButton>
-        </div>
-
-        <ComboboxOptions
-          anchor="bottom"
-          transition
-          className={clsx(
-            "w-[var(--input-width)] rounded-xl border border-black/5 bg-black/5 p-1 [--anchor-gap:var(--spacing-1)] empty:invisible",
-            "transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0"
-          )}
-        >
-          {filteredPeople.map((person) => (
-            <ComboboxOption
-              key={person.id}
-              value={person}
-              className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-black/10"
-            >
-              <CheckIcon className="invisible size-4 fill-black group-data-[selected]:visible" />
-              <div className="text-sm/6 text-black">{person.name}</div>
-            </ComboboxOption>
-          ))}
-        </ComboboxOptions>
-      </Combobox>
-    </div>
+    <Combobox
+      value={selectedPerson}
+      onChange={setSelectedPerson}
+      onClose={() => setQuery("")}
+    >
+      <ComboboxInput
+        aria-label="Assignee"
+        displayValue={(person) => person?.name}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      <ComboboxOptions anchor="bottom" className="border empty:invisible">
+        {filteredPeople.map((person) => (
+          <ComboboxOption
+            key={person.id}
+            value={person}
+            className="data-[focus]:bg-blue-100"
+          >
+            {person.name}
+          </ComboboxOption>
+        ))}
+      </ComboboxOptions>
+    </Combobox>
   );
 }
