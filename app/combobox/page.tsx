eac1b0c7cced3fb700c965652d5943b1eac0bd40
Changnam Go <changnam.go@gmail.com>
2025-02-27 23:36:00 +0900

    headlessui combox immediate 옵션 사용하여 inputbox 에 focus 받았을때 자동으로 dropdown연다

--- app/combobox/page.tsx
+++ app/combobox/page.tsx
@@ -1,45 +1,56 @@
 "use client";
 
-import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/react'
-import { useState } from 'react'
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
+import { useState } from "react";
 
 const people = [
-  { id: 1, name: 'Durward Reynolds' },
-  { id: 2, name: 'Kenton Towne' },
-  { id: 3, name: 'Therese Wunsch' },
-  { id: 4, name: 'Benedict Kessler' },
-  { id: 5, name: 'Katelyn Rohan' },
-]
+  { id: 1, name: "Durward Reynolds" },
+  { id: 2, name: "Kenton Towne" },
+  { id: 3, name: "Therese Wunsch" },
+  { id: 4, name: "Benedict Kessler" },
+  { id: 5, name: "Katelyn Rohan" },
+];
 
 export default function Example() {
-  const [selectedPeople, setSelectedPeople] = useState([people[0], people[1]])
-  const [query, setQuery] = useState('')
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+  const [query, setQuery] = useState("");
 
   const filteredPeople =
-    query === ''
+    query === ""
       ? people
       : people.filter((person) => {
-          return person.name.toLowerCase().includes(query.toLowerCase())
-        })
+          return person.name.toLowerCase().includes(query.toLowerCase());
+        });
 
   return (
-    <Combobox multiple value={selectedPeople} onChange={setSelectedPeople} onClose={() => setQuery('')}>
-      {selectedPeople.length > 0 && (
-        <ul>
-          {selectedPeople.map((person) => (
-            <li key={person.id}>{person.name}</li>
-          ))}
-        </ul>
-      )}
-      <ComboboxInput aria-label="Assignees" onChange={(event) => setQuery(event.target.value)} 
-        className="border" />
+    <Combobox
+      immediate
+      value={selectedPerson}
+      onChange={setSelectedPerson}
+      onClose={() => setQuery("")}
+    >
+      <ComboboxInput
+        aria-label="Assignee"
+        displayValue={(person) => person?.name}
+        onChange={(event) => setQuery(event.target.value)}
+        className="border"
+      />
       <ComboboxOptions anchor="bottom" className="border empty:invisible">
         {filteredPeople.map((person) => (
-          <ComboboxOption key={person.id} value={person} className="data-[focus]:bg-blue-100">
+          <ComboboxOption
+            key={person.id}
+            value={person}
+            className="data-[focus]:bg-blue-100"
+          >
             {person.name}
           </ComboboxOption>
         ))}
       </ComboboxOptions>
     </Combobox>
-  )
+  );
 }
