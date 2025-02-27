a29b6eef1939540e32566c75cd64c31da6696219
Changnam Go <changnam.go@gmail.com>
2025-02-27 23:26:34 +0900

    headlessui combox bind 된 value 가 object 가 아닌 string 이면 combobox 의 displayValue 생략 가능

--- app/combobox/page.tsx
+++ app/combobox/page.tsx
@@ -1,9 +1,54 @@
-import DepartmentPicker from "@/components/department-picker";
+"use client";
 
-export default function ComboboxPage() {
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
+import { useState } from "react";
+
+const people = [
+  "Durward Reynolds",
+  "Kenton Towne",
+  "Therese Wunsch",
+  "Benedict Kessler",
+  "Katelyn Rohan",
+];
+
+export default function Example() {
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+  const [query, setQuery] = useState("");
+
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) => {
+          return person.toLowerCase().includes(query.toLowerCase());
+        });
+
   return (
-    <div>
-      <DepartmentPicker selectedDepartment={{name: "Marketing"}}  />
-    </div>
+    <Combobox
+      value={selectedPerson}
+      onChange={setSelectedPerson}
+      onClose={() => setQuery("")}
+    >
+      <ComboboxInput
+        aria-label="Assignee"
+        onChange={(event) => setQuery(event.target.value)}
+        className="border"
+      />
+      <ComboboxOptions anchor="bottom" className="border empty:invisible">
+        {filteredPeople.map((person) => (
+          <ComboboxOption
+            key={person}
+            value={person}
+            className="data-[focus]:bg-blue-100"
+          >
+            {person}
+          </ComboboxOption>
+        ))}
+      </ComboboxOptions>
+    </Combobox>
   );
 }
