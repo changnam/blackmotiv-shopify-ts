2b4fc5d5cd8bef53621b64b2f4df10a72d0009a3
Changnam Go <changnam.go@gmail.com>
2025-02-27 20:23:13 +0900

    headlessui checkbox 를 Field로 감싸고 , label 을 추가

--- app/checkbox/page.tsx
+++ app/checkbox/page.tsx
@@ -1,41 +1,32 @@
 "use client";
 
-import { Checkbox } from "@headlessui/react";
-import clsx from "clsx";
-import { Fragment, useState } from "react";
+import { useState } from "react";
+import { Checkbox, Field, Label } from "@headlessui/react";
 
 export default function Example() {
-  const [enabled, setEnabled] = useState(true);
+  const [enabled, setEnabled] = useState(false);
 
   return (
-    <Checkbox checked={enabled} onChange={setEnabled} as={Fragment}>
-      {({ checked, disabled }) => (
-        <span
-          className={clsx(
-            "block size-4 rounded border",
-            !checked && "bg-white",
-            checked && !disabled && "bg-blue-500",
-            checked && disabled && "bg-gray-500",
-            disabled && "cursor-not-allowed opacity-50"
-          )}
+    <Field className="flex items-center gap-2">
+      <Checkbox
+        checked={enabled}
+        onChange={setEnabled}
+        className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
+      >
+        <svg
+          className="stroke-white opacity-0 group-data-[checked]:opacity-100"
+          viewBox="0 0 14 14"
+          fill="none"
         >
-          <svg
-            className={clsx(
-              "stroke-white",
-              checked ? "opacity-100" : "opacity-0"
-            )}
-            viewBox="0 0 14 14"
-            fill="none"
-          >
-            <path
-              d="M3 8L6 11L11 3.5"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </span>
-      )}
-    </Checkbox>
+          <path
+            d="M3 8L6 11L11 3.5"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </Checkbox>
+      <Label>Enable beta features</Label>
+    </Field>
   );
 }
