188e4852bce29f842a13d98e5841dab3051883f8
Changnam Go <changnam.go@gmail.com>
2025-02-27 20:56:18 +0900

    headlessui checkbox transition 은 simple 하게 적용. duration 시간을 custom 값으로 줌

--- app/checkbox/page.tsx
+++ app/checkbox/page.tsx
@@ -1,27 +1,29 @@
+"use client";
+
 import { Checkbox } from "@headlessui/react";
+import { useState } from "react";
 
 export default function Example() {
+  const [enabled, setEnabled] = useState(false);
+
   return (
-    <form action="http://localhost:8080/api/posts/1/like" method="post">
-      <Checkbox
-        defaultChecked={true}
-        name="terms-of-service"
-        className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
+    <Checkbox
+      checked={enabled}
+      onChange={setEnabled}
+      className="group block size-4 rounded border bg-white transition duration-[3s,5s] data-[checked]:bg-blue-500"
+    >
+      <svg
+        className="stroke-white opacity-0 transition group-data-[checked]:opacity-100"
+        viewBox="0 0 14 14"
+        fill="none"
       >
-        <svg
-          className="stroke-white opacity-0 group-data-[checked]:opacity-100"
-          viewBox="0 0 14 14"
-          fill="none"
-        >
-          <path
-            d="M3 8L6 11L11 3.5"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </Checkbox>
-      <button>Submit</button>
-    </form>
+        <path
+          d="M3 8L6 11L11 3.5"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </Checkbox>
   );
 }
