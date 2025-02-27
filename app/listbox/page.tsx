ed5bf8485b887fd8066d89c56d4d4653f638a4b8
Changnam Go <changnam.go@gmail.com>
2025-02-28 08:04:58 +0900

    headlessui listbox , label, description 추가

--- app/listbox/page.tsx
+++ app/listbox/page.tsx
@@ -1,61 +1,47 @@
 "use client";
 
 import {
+  Description,
+  Field,
+  Label,
   Listbox,
   ListboxButton,
   ListboxOption,
   ListboxOptions,
 } from "@headlessui/react";
-import { CheckIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
-import clsx from "clsx";
 import { useState } from "react";
 
 const people = [
-  { id: 1, name: "Tom Cook" },
-  { id: 2, name: "Wade Cooper" },
-  { id: 3, name: "Tanya Fox" },
-  { id: 4, name: "Arlene Mccoy" },
-  { id: 5, name: "Devon Webb" },
+  { id: 1, name: "Durward Reynolds" },
+  { id: 2, name: "Kenton Towne" },
+  { id: 3, name: "Therese Wunsch" },
+  { id: 4, name: "Benedict Kessler" },
+  { id: 5, name: "Katelyn Rohan" },
 ];
 
 export default function Example() {
-  const [selected, setSelected] = useState(people[1]);
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
 
   return (
-    <div className="mx-auto h-screen w-52 pt-20 border border-gray-300">
-      <Listbox value={selected} onChange={setSelected}>
-        <ListboxButton
-          className={clsx(
-            "relative block w-full rounded-lg bg-black/5 py-1.5 pr-8 pl-3 text-left text-sm/6 text-black",
-            "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25"
-          )}
-        >
-          {selected.name}
-          <ChevronDownIcon
-            className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-black/60"
-            aria-hidden="true"
-          />
-        </ListboxButton>
-        <ListboxOptions
-          anchor="bottom"
-          transition
-          className={clsx(
-            "w-[var(--button-width)] rounded-xl border border-black/5 bg-black/5 p-1 [--anchor-gap:var(--spacing-1)] focus:outline-none",
-            "transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0"
-          )}
-        >
+    <Field disabled>
+      <Label>Assignee:</Label>
+      <Description>
+        This person will have full access to this project.
+      </Description>
+      <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+        <ListboxButton>{selectedPerson.name}</ListboxButton>
+        <ListboxOptions anchor="bottom">
           {people.map((person) => (
             <ListboxOption
-              key={person.name}
+              key={person.id}
               value={person}
-              className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-black/10"
+              className="data-[focus]:bg-blue-100"
             >
-              <CheckIcon className="invisible size-4 fill-black group-data-[selected]:visible" />
-              <div className="text-sm/6 text-black">{person.name}</div>
+              {person.name}
             </ListboxOption>
           ))}
         </ListboxOptions>
       </Listbox>
-    </div>
+    </Field>
   );
 }
