951ceeb2fea0536ba115fa5d491e0295f6222c0e
Changnam Go <changnam.go@gmail.com>
2025-02-27 20:29:28 +0900

    headlessui checkbox Field 에 disabled 추가시 , 하위 항목 모두 disabled 됨

--- app/checkbox/page.tsx
+++ app/checkbox/page.tsx
@@ -7,15 +7,15 @@ export default function Example() {
   const [enabled, setEnabled] = useState(false);
 
   return (
-    <Field>
-      <Label>Enable beta features</Label>
-      <Description>
+    <Field disabled>
+      <Label className="data-[disabled]:opacity-50">Enable beta features</Label>
+      <Description className="data-[disabled]:opacity-50">
         This will give you early access to new features we're developing.
       </Description>
       <Checkbox
         checked={enabled}
         onChange={setEnabled}
-        className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
+        className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500 data-[disabled]:cursor-not-allowed data-[disabled]:opacity-50 data-[checked]:data-[disabled]:bg-gray-500"
       >
         <svg
           className="stroke-white opacity-0 group-data-[checked]:opacity-100"
