233d855cb94a6397f3fa03d3ba4e643d2f86f69e
Changnam Go <changnam.go@gmail.com>
2025-02-28 07:36:22 +0900

    headlessui fiedlset basic . fieldset 과 legend 이용하여 field grouping

--- app/fieldset/page.tsx
+++ app/fieldset/page.tsx
@@ -1,5 +1,4 @@
 import {
-  Description,
   Field,
   Fieldset,
   Input,
@@ -8,67 +7,27 @@ import {
   Select,
   Textarea,
 } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import clsx from "clsx";
 
 export default function Example() {
   return (
-    <div className="w-full max-w-lg px-4">
-      <Fieldset className="space-y-6 rounded-xl bg-black/5 p-6 sm:p-10">
-        <Legend className="text-base/7 font-semibold text-black">
-          Shipping details
-        </Legend>
-        <Field>
-          <Label className="text-sm/6 font-medium text-black">
-            Street address
-          </Label>
-          <Input
-            className={clsx(
-              "mt-3 block w-full rounded-lg border-none bg-black/5 py-1.5 px-3 text-sm/6 text-black",
-              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25"
-            )}
-          />
-        </Field>
-        <Field>
-          <Label className="text-sm/6 font-medium text-black">Country</Label>
-          <Description className="text-sm/6 text-black/50">
-            We currently only ship to North America.
-          </Description>
-          <div className="relative">
-            <Select
-              className={clsx(
-                "mt-3 block w-full appearance-none rounded-lg border-none bg-black/5 py-1.5 px-3 text-sm/6 text-black",
-                "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25",
-                // Make the text of each option black on Windows
-                "*:text-black"
-              )}
-            >
-              <option>Canada</option>
-              <option>Mexico</option>
-              <option>United States</option>
-            </Select>
-            <ChevronDownIcon
-              className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-black/60"
-              aria-hidden="true"
-            />
-          </div>
-        </Field>
-        <Field>
-          <Label className="text-sm/6 font-medium text-black">
-            Delivery notes
-          </Label>
-          <Description className="text-sm/6 text-black/50">
-            If you have a tiger, we'd like to know about it.
-          </Description>
-          <Textarea
-            className={clsx(
-              "mt-3 block w-full resize-none rounded-lg border-none bg-black/5 py-1.5 px-3 text-sm/6 text-black",
-              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25"
-            )}
-            rows={3}
-          />
-        </Field>
-      </Fieldset>
-    </div>
+    <Fieldset className="space-y-8 border border-gray-300">
+      <Legend className="text-lg font-bold">Shipping details</Legend>
+      <Field>
+        <Label className="block">Street address</Label>
+        <Input className="mt-1 block" name="address" />
+      </Field>
+      <Field>
+        <Label className="block">Country</Label>
+        <Select className="mt-1 block" name="country">
+          <option>Canada</option>
+          <option>Mexico</option>
+          <option>United States</option>
+        </Select>
+      </Field>
+      <Field>
+        <Label className="block">Delivery notes</Label>
+        <Textarea className="mt-1 block" name="notes" />
+      </Field>
+    </Fieldset>
   );
 }
