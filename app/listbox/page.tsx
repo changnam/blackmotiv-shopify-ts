7c470d73b07716509999535acd12d981b54e75b1
Changnam Go <changnam.go@gmail.com>
2025-02-28 09:25:27 +0900

    headlessui listbox , custom listbox 생성 방법

--- app/listbox/page.tsx
+++ app/listbox/page.tsx
@@ -4,39 +4,60 @@ import {
   ListboxButton,
   ListboxOption,
   ListboxOptions,
+  ListboxSelectedOption,
 } from "@headlessui/react";
-import { forwardRef, useState } from "react";
+import { Fragment, useState } from "react";
 
 const people = [
   { id: 1, name: "Durward Reynolds" },
   { id: 2, name: "Kenton Towne" },
   { id: 3, name: "Therese Wunsch" },
   { id: 4, name: "Benedict Kessler" },
-  { id: 5, name: "Katelyn Rohan" },
+  { id: 5, name: "Katelyn blablabla" },
 ];
 
-let MyCustomButton = forwardRef(function (props, ref) {
-  return <button className="bg-green-200" ref={ref} {...props} />;
-});
-
 export default function Example() {
-  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+  const [selectedPerson, setSelectedPerson] = useState(null);
 
   return (
-    <Listbox value={selectedPerson} onChange={setSelectedPerson}>
-      <ListboxButton as={MyCustomButton}>{selectedPerson.name}</ListboxButton>
-      <ListboxOptions anchor="bottom" as="ul">
-        {people.map((person) => (
-          <ListboxOption
-            as="li"
-            key={person.id}
-            value={person}
-            className="data-[focus]:bg-blue-100"
-          >
-            {person.name}
-          </ListboxOption>
-        ))}
-      </ListboxOptions>
+    <MyListbox
+      value={selectedPerson}
+      onChange={setSelectedPerson}
+      placeholder="Select a person&hellip;"
+    >
+      {people.map((person) => (
+        <MyListboxOption key={person.id} value={person}>
+          {person.name}
+        </MyListboxOption>
+      ))}
+    </MyListbox>
+  );
+}
+
+function MyListbox({ placeholder, children, ...props }) {
+  return (
+    <Listbox {...props}>
+      <ListboxButton>
+        <ListboxSelectedOption
+          options={children}
+          placeholder={<span className="opacity-50">{placeholder}</span>}
+        />
+      </ListboxButton>
+      <ListboxOptions anchor="bottom">{children}</ListboxOptions>
     </Listbox>
+  );
+}
+
+function MyListboxOption({ children, ...props }) {
+  return (
+    <ListboxOption as={Fragment} {...props}>
+      {({ selectedOption }) => {
+        return selectedOption ? (
+          children
+        ) : (
+          <div className="data-[focus]:bg-blue-100">{children}</div>
+        );
+      }}
+    </ListboxOption>
   );
 }
