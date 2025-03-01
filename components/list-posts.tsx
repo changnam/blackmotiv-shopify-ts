d6739685765ce27c6fdc15d910cedf3ff5b93c66
Changnam Go <changnam.go@gmail.com>
2025-03-02 01:18:57 +0900

    server action deletePostApi 구현, render 시 함수 호출 안되도록 주의 요망!, <button onClick={() => deletePostApi(post.id)}>Delete</button>   , 그냥 {deletePostApi(post.id)} 로 하면 호출되어버림

--- components/list-posts.tsx
+++ components/list-posts.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-export default function ListPosts({ posts }: { posts: any[] }) {
+export default function ListPosts({ posts, deletePostApi }: { posts: any[], deletePostApi: any }) {
     console.log("ListPosts started.");
   return (
     <ul>
       {posts.map((post) => (
-        <li key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
+        <li key={post.id} className="flex space-x-4">
+          <h2 className="w-[30px]">{post.title}</h2>
+          <p className="w-[50px]">{post.content}</p>
+          <button onClick={() => deletePostApi(post.id)}>Delete</button>
         </li>
       ))}
     </ul>
