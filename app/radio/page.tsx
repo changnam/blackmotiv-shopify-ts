8c646f6637dfb06240e2855378d24f7dc31b23ed
Changnam Go <changnam.go@gmail.com>
2025-03-02 16:15:46 +0900

    radio 데이터 속성에 따라서 description 추가

--- app/radio/page.tsx
+++ app/radio/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import { Field, Label, Radio, RadioGroup } from "@headlessui/react";
+import {
+  Description,
+  Field,
+  Label,
+  Radio,
+  RadioGroup,
+} from "@headlessui/react";
 import { useState } from "react";
 
 const plans = [
-  { name: "Startup", available: true },
-  { name: "Business", available: true },
-  { name: "Enterprise", available: false },
+  { name: "Startup", description: "12GB, 6 CPUs, 256GB SSD disk" },
+  { name: "Business", description: "16GB, 8 CPUs, 512GB SSD disk" },
+  { name: "Enterprise", description: "32GB, 12 CPUs, 1TB SSD disk" },
 ];
 
 export default function Example() {
@@ -19,18 +25,17 @@ export default function Example() {
       aria-label="Server size"
     >
       {plans.map((plan) => (
-        <Field
-          key={plan.name}
-          disabled={!plan.available}
-          className="flex items-center gap-2"
-        >
+        <Field key={plan.name} className="flex items-baseline gap-2">
           <Radio
             value={plan}
-            className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400 data-[disabled]:bg-gray-100"
+            className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400"
           >
             <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible" />
           </Radio>
-          <Label className="data-[disabled]:opacity-50">{plan.name}</Label>
+          <div>
+            <Label>{plan.name}</Label>
+            <Description className="opacity-50">{plan.description}</Description>
+          </div>
         </Field>
       ))}
     </RadioGroup>
