9f490af5ed6acacadda1f0d4515f4ec0fe8f83d8
Changnam Go <changnam.go@gmail.com>
2025-03-02 16:47:51 +0900

    radiogroup option 이 object 이면 by 를 줘야 한다. 기본은 id 를 사용 , by='name' 을 주거나, custom comparison 함수를 줄수 있다

--- app/radio/page.tsx
+++ app/radio/page.tsx
@@ -1,36 +1,39 @@
 "use client";
 
-import {
-  Field,
-  Fieldset,
-  Label,
-  Legend,
-  Radio,
-  RadioGroup,
-} from "@headlessui/react";
+import { Field, Label, Radio, RadioGroup } from "@headlessui/react";
 import { useState } from "react";
 
-const plans = ["Startup", "Business", "Enterprise"];
+type planType = {
+  id: number;
+  name: string;
+  available: boolean;}
+
+const plans = [
+  { id: 1, name: "Startup", available: true },
+  { id: 2, name: "Business", available: true },
+  { id: 3, name: "Enterprise", available: false },
+];
+
+ function comparePlans(a: planType, b: planType) {
+  return a.name.toLowerCase() === b.name.toLowerCase();
+}
 
 export default function Example() {
   const [selected, setSelected] = useState(plans[0]);
 
   return (
-    <form action="http://localhost:8080/api/posts/1/like" method="post">
-      <Fieldset>
-        <Legend>Server size</Legend>
-        <RadioGroup name="plan" defaultValue={plans[0]} onChange={setSelected}>
-          {plans.map((plan) => (
-            <Field key={plan} className="flex items-baseline gap-2">
-              <Radio value={plan} className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400">
-                <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible" />
-              </Radio>
-              <Label>{plan}</Label>
-            </Field>
-          ))}
-        </RadioGroup>
-      </Fieldset>
-      <button>Submit</button>
-    </form>
+    <RadioGroup
+      value={selected}
+      by={comparePlans}
+      onChange={setSelected}
+      aria-label="Server size"
+    >
+      {plans.map((plan) => (
+        <Field key={plan.id}>
+          <Radio value={plan} disabled={!plan.available} />
+          <Label>{plan.name}</Label>
+        </Field>
+      ))}
+    </RadioGroup>
   );
 }
