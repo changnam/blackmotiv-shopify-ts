4a2555484bd32401bd49b604c1e77934a75f1dbe
Changnam Go <changnam.go@gmail.com>
2025-03-02 16:27:50 +0900

    radio form 에서 사용하기 hidden input 으로 plan=startup 형식으로 값을 전달

--- app/radio/page.tsx
+++ app/radio/page.tsx
@@ -1,43 +1,36 @@
 "use client";
 
 import {
-  Description,
   Field,
+  Fieldset,
   Label,
+  Legend,
   Radio,
   RadioGroup,
 } from "@headlessui/react";
 import { useState } from "react";
 
-const plans = [
-  { name: "Startup", description: "12GB, 6 CPUs, 256GB SSD disk" },
-  { name: "Business", description: "16GB, 8 CPUs, 512GB SSD disk" },
-  { name: "Enterprise", description: "32GB, 12 CPUs, 1TB SSD disk" },
-];
+const plans = ["Startup", "Business", "Enterprise"];
 
 export default function Example() {
-  let [selected, setSelected] = useState(plans[0]);
+  const [selected, setSelected] = useState(plans[0]);
 
   return (
-    <RadioGroup
-      value={selected}
-      onChange={setSelected}
-      aria-label="Server size"
-    >
-      {plans.map((plan) => (
-        <Field key={plan.name} className="flex items-baseline gap-2">
-          <Radio
-            value={plan}
-            className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400"
-          >
-            <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible" />
-          </Radio>
-          <div>
-            <Label>{plan.name}</Label>
-            <Description className="opacity-50">{plan.description}</Description>
-          </div>
-        </Field>
-      ))}
-    </RadioGroup>
+    <form action="http://localhost:8080/api/posts/1/like" method="post">
+      <Fieldset>
+        <Legend>Server size</Legend>
+        <RadioGroup name="plan" value={selected} onChange={setSelected}>
+          {plans.map((plan) => (
+            <Field key={plan} className="flex items-baseline gap-2">
+              <Radio value={plan} className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400">
+                <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible" />
+              </Radio>
+              <Label>{plan}</Label>
+            </Field>
+          ))}
+        </RadioGroup>
+      </Fieldset>
+      <button>Submit</button>
+    </form>
   );
 }
