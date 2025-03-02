859416dc16f1e66f0dbf11a1f099ab4584a9486f
Changnam Go <changnam.go@gmail.com>
2025-03-02 16:08:12 +0900

    radio 데이터 속성에 따라서 disabled 로 스타일링

--- app/radio/page.tsx
+++ app/radio/page.tsx
@@ -3,7 +3,11 @@
 import { Field, Label, Radio, RadioGroup } from "@headlessui/react";
 import { useState } from "react";
 
-const plans = ["Startup", "Business", "Enterprise"];
+const plans = [
+  { name: "Startup", available: true },
+  { name: "Business", available: true },
+  { name: "Enterprise", available: false },
+];
 
 export default function Example() {
   let [selected, setSelected] = useState(plans[0]);
@@ -15,14 +19,18 @@ export default function Example() {
       aria-label="Server size"
     >
       {plans.map((plan) => (
-        <Field key={plan} className="flex items-center gap-2">
+        <Field
+          key={plan.name}
+          disabled={!plan.available}
+          className="flex items-center gap-2"
+        >
           <Radio
             value={plan}
-            className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400"
+            className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400 data-[disabled]:bg-gray-100"
           >
             <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible" />
           </Radio>
-          <Label>{plan}</Label>
+          <Label className="data-[disabled]:opacity-50">{plan.name}</Label>
         </Field>
       ))}
     </RadioGroup>
