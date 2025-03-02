81ee57b84696e02ac5926416a6f78cdd574027f8
Changnam Go <changnam.go@gmail.com>
2025-03-02 17:41:08 +0900

    Menu component dropdown 영역의 positioning 과 gap 을 object 로 조정

--- app/dropdown/page.tsx
+++ app/dropdown/page.tsx
@@ -1,63 +1,27 @@
 "use client";
 
-import {
-  Menu,
-  MenuButton,
-  MenuHeading,
-  MenuItem,
-  MenuItems,
-  MenuSection,
-  MenuSeparator,
-} from "@headlessui/react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 export default function Example() {
-  function showSettingsDialog() {
-    alert("Open settings dialog!");
-  }
-
   return (
     <Menu>
       <MenuButton>My account</MenuButton>
-      <MenuItems anchor="bottom">
-        <MenuSection>
-          <MenuHeading className="text-sm opacity-50">Settings</MenuHeading>
-          <MenuItem>
-            <button
-              onClick={showSettingsDialog}
-              className="block w-full text-left data-[focus]:bg-blue-100"
-            >
-              Settings
-            </button>
-          </MenuItem>
-          <MenuItem>
-            <a className="block data-[focus]:bg-blue-100" href="/support">
-              Support
-            </a>
-          </MenuItem>
-        </MenuSection>
-        <MenuSeparator className="my-1 h-px bg-black" />
-        <MenuSection>
-          <MenuHeading className="text-sm opacity-50">License</MenuHeading>
-          <MenuItem>
-            <a className="block data-[focus]:bg-blue-100" href="/license">
-              License
-            </a>
-          </MenuItem>
-        </MenuSection>
-        <MenuSeparator className="my-1 h-px bg-black" />
-        <MenuSection>
-          <MenuHeading className="text-sm opacity-50"></MenuHeading>
-          <form action="/logout" method="post">
-            <MenuItem>
-              <button
-                type="submit"
-                className="block w-full text-left data-[focus]:bg-blue-100"
-              >
-                Sign out
-              </button>
-            </MenuItem>
-          </form>
-        </MenuSection>
+      <MenuItems anchor={{ to: "bottom start", gap: "4px" }}>
+        <MenuItem>
+          <a className="block data-[focus]:bg-blue-100" href="/settings">
+            Settings
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a className="block data-[focus]:bg-blue-100" href="/support">
+            Support
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a className="block data-[focus]:bg-blue-100" href="/license">
+            License
+          </a>
+        </MenuItem>
       </MenuItems>
     </Menu>
   );
