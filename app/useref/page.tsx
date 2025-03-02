7fefe3a8019f4d7d57bc69c5976771420e010a20
Changnam Go <changnam.go@gmail.com>
2025-03-02 12:59:38 +0900

    useref 기존 값 저장. mount 이후에 값을 변경하므로 화면에는 이전 값이 보인다

--- app/useref/page.tsx
+++ app/useref/page.tsx
@@ -1,24 +1,19 @@
 "use client";
-import { useRef, useEffect } from "react";
-export default function FocusInput() {
-  console.log("FocusInput started.");
-  const inputRef = useRef<HTMLInputElement>(null);
-  const clickCounter = useRef(0);
-
-  const increamentCounter = () => {
-    clickCounter.current += 1;
-    inputRef.current.value = String(clickCounter.current);
-  };
+import { useRef, useState, useEffect } from "react";
+export default function PreviousStateExample() {
+    console.log("PreviousStateExample started.");
+  const [count, setCount] = useState(0);
+  const prevCountRef = useRef<number | null>(null);
 
   useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, []);
+    prevCountRef.current = count; // mount 된 이후에 실행되므로 화면에 표시된 값은 count: 0, prevCount: N/A 이고, 이후에 여기 값이 0으로 바뀜. 즉, mount 이후에 값을 조작하지만 화면에는 영향을 미치지 않음
+  }, [count]);
 
   return (
-    <div onClick={increamentCounter} className="border p-4 h-120px bg-green-300">
-      <input ref={inputRef} type="text" placeholder="Focus me!" />
+    <div>
+      <p>Current Count: {count} </p>
+      <p>Previous Count: {prevCountRef.current ?? "N/A"}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 }
