08f912f462347e00db4497e01edb955451981502
Changnam Go <changnam.go@gmail.com>
2025-03-02 16:30:37 +0900

    radio form 에서 사용하기, uncontrolled value 로 사용시 defaultValue 사용하여 초기값 설정

--- app/radio/page.tsx
+++ app/radio/page.tsx
@@ -19,7 +19,7 @@ export default function Example() {
     <form action="http://localhost:8080/api/posts/1/like" method="post">
       <Fieldset>
         <Legend>Server size</Legend>
-        <RadioGroup name="plan" value={selected} onChange={setSelected}>
+        <RadioGroup name="plan" defaultValue={plans[0]} onChange={setSelected}>
           {plans.map((plan) => (
             <Field key={plan} className="flex items-baseline gap-2">
               <Radio value={plan} className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400">
