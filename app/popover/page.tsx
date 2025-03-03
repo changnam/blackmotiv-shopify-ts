4488414f7cb4f9414e23345ddf80546cdb399ca9
Changnam Go <changnam.go@gmail.com>
2025-03-03 17:38:59 +0900

    popover render prop open 이용하여 스타일링. Popover 는 기본 Fragment 로 render 되며 key, children 만 속성으로 가질수 있다. 따라서 as 사용하여 div 로 render 할것. 'use client' 주어 client component로 만들것

--- app/popover/page.tsx
+++ app/popover/page.tsx
@@ -1,18 +1,26 @@
+
+"use client";
+
 import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import clsx from "clsx";
 
 export default function Example() {
   return (
-    <Popover className="group">
-      <PopoverButton className="flex items-center gap-2">
-        Solutions
-        <ChevronDownIcon className="size-5 group-data-[open]:rotate-180" />
-      </PopoverButton>
-      <PopoverPanel anchor="bottom" className="flex flex-col">
-        <a href="/insights">Insights</a>
-        <a href="/automations">Automations</a>
-        <a href="/reports">Reports</a>
-      </PopoverPanel>
+    <Popover as="div">
+      {({ open }) => (
+        <>
+          <PopoverButton className="flex items-center gap-2">
+            Solutions
+            <ChevronDownIcon className={clsx("size-5", open && "rotate-180")} />
+          </PopoverButton>
+          <PopoverPanel anchor="bottom" className="flex flex-col">
+            <a href="/insights">Insights</a>
+            <a href="/automations">Automations</a>
+            <a href="/reports">Reports</a>
+          </PopoverPanel>
+        </>
+      )}
     </Popover>
   );
 }
