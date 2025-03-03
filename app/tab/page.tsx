1c7b5015e947c054eff550a70eb2ae213be2cdc0
Changnam Go <changnam.go@gmail.com>
2025-03-03 18:20:56 +0900

    headlessui tab manual 속성으로 tab 간 이동은 가능하지만 Enter 를 해야 해당 탭의 내용이 선택되어짐, 마우스로 클릭시에는 내용까지 바로 선택됨

--- app/tab/page.tsx
+++ app/tab/page.tsx
@@ -5,51 +5,18 @@ import clsx from "clsx";
 import { Fragment } from "react";
 
 export default function Example() {
-  return (
-    <TabGroup vertical>
-      <TabList as="aside" className="flex flex-col">
-        <Tab as={Fragment}>
-          {({ hover, selected }) => (
-            <button
-              className={clsx(
-                hover && "underline",
-                selected && "bg-blue-500 text-white"
-              )}
-            >
-              Tab 1
-            </button>
-          )}
-        </Tab>
-        <Tab as={Fragment} disabled className="disabled:opacity-50">
-          {({ hover, selected }) => (
-            <button
-              className={clsx(
-                hover && "underline",
-                selected && "bg-blue-500 text-white"
-              )}
-            >
-              Tab 2
-            </button>
-          )}
-        </Tab>
-        <Tab as={Fragment}>
-          {({ hover, selected }) => (
-            <button
-              className={clsx(
-                hover && "underline",
-                selected && "bg-blue-500 text-white"
-              )}
-            >
-              Tab 3
-            </button>
-          )}
-        </Tab>
-      </TabList>
-      <TabPanels as="section">
-        <TabPanel>Content 1</TabPanel>
-        <TabPanel>Content 2</TabPanel>
-        <TabPanel>Content 3</TabPanel>
-      </TabPanels>
-    </TabGroup>
-  );
+ return (
+   <TabGroup manual>
+     <TabList>
+       <Tab>Tab 1</Tab>
+       <Tab>Tab 2</Tab>
+       <Tab>Tab 3</Tab>
+     </TabList>
+     <TabPanels>
+       <TabPanel>Content 1</TabPanel>
+       <TabPanel>Content 2</TabPanel>
+       <TabPanel>Content 3</TabPanel>
+     </TabPanels>
+   </TabGroup>
+ );
 }
