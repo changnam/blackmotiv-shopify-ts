95d6e966b4c4e7267c0821069bebad108b877988
Changnam Go <changnam.go@gmail.com>
2025-03-03 22:57:26 +0900

    popover build error 해결

--- app/popover/page.tsx
+++ app/popover/page.tsx
@@ -3,14 +3,10 @@
 import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 import { forwardRef } from "react";
 
-let MyCustomButton = forwardRef(function (props, ref) {
-  return <button className="..." ref={ref} {...props} />;
-});
-
 export default function Example() {
   return (
     <Popover as="nav">
-      <PopoverButton as={MyCustomButton}>Solutions</PopoverButton>
+      <PopoverButton as="button">Solutions</PopoverButton>
       <PopoverPanel as="form">
         {({ close }) => (
           <button
