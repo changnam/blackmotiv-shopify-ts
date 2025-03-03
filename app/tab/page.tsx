0db53f9cd0bb24e56febc076c9e182fa65f924a8
Changnam Go <changnam.go@gmail.com>
2025-03-03 18:29:00 +0900

    headlessui tab 이동시 선택된 index 는 내부적으로 관리되는데, 직접 상태정보를 이용하여 관리하고자할때

--- app/tab/page.tsx
+++ app/tab/page.tsx
@@ -1,28 +1,23 @@
 "use client";
 
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
-import clsx from "clsx";
-import { Fragment } from "react";
+import { useState } from "react";
 
 export default function Example() {
- return (
-   <TabGroup
-     manual
-     defaultIndex={2}
-     onChange={(index) => {
-       console.log("Changed selected tab to:", index);
-     }}
-   >
-     <TabList>
-       <Tab>Tab 1</Tab>
-       <Tab>Tab 2</Tab>
-       <Tab>Tab 3</Tab>
-     </TabList>
-     <TabPanels>
-       <TabPanel>Content 1</TabPanel>
-       <TabPanel>Content 2</TabPanel>
-       <TabPanel>Content 3</TabPanel>
-     </TabPanels>
-   </TabGroup>
- );
+const [selectedIndex, setSelectedIndex] = useState(0);
+
+return (
+  <TabGroup selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+    <TabList>
+      <Tab>Tab 1</Tab>
+      <Tab>Tab 2</Tab>
+      <Tab>Tab 3</Tab>
+    </TabList>
+    <TabPanels>
+      <TabPanel>Content 1</TabPanel>
+      <TabPanel>Content 2</TabPanel>
+      <TabPanel>Content 3</TabPanel>
+    </TabPanels>
+  </TabGroup>
+);
 }
