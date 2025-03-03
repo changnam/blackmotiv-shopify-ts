87eaa244f02b1236df247ef74aa68da4e51094c6
Changnam Go <changnam.go@gmail.com>
2025-03-03 17:56:56 +0900

    popover useClose 사용하여 수동으로 팝업 닫기

--- app/popover/page.tsx
+++ app/popover/page.tsx
@@ -1,37 +1,37 @@
-import { Popover, PopoverBackdrop, PopoverButton, PopoverGroup, PopoverPanel } from "@headlessui/react";
+"use client";
+
+import {
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+  useClose,
+} from "@headlessui/react";
+
+function MySearchForm() {
+  let close = useClose();
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        /* Perform search... */
+        close();
+      }}
+    >
+      <input type="search" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 export default function Example() {
   return (
-    <PopoverGroup>
-      <Popover>
-        <PopoverButton>Product</PopoverButton>
-        <PopoverPanel>
-          <div>상품품</div>
-          {/* ... */}
-        </PopoverPanel>
-      </Popover>
-
-      <Popover>
-        <PopoverButton>Solutions</PopoverButton>
-        <PopoverPanel>
-          <div>솔루션션</div>
-          {/* ... */}
-        </PopoverPanel>
-      </Popover>
-
-      <Popover>
-        <PopoverButton>Solutions</PopoverButton>
-        <PopoverPanel
-          anchor="bottom"
-          transition
-          className="flex origin-top flex-col transition duration-200 ease-out data-[closed]:scale-95 data-[closed]:opacity-0"
-        >
-          <a href="/analytics">Analytics</a>
-          <a href="/engagement">Engagement</a>
-          <a href="/security">Security</a>
-          <a href="/integrations">Integrations</a>
-        </PopoverPanel>
-      </Popover>
-    </PopoverGroup>
+    <Popover>
+      <PopoverButton>Filters</PopoverButton>
+      <PopoverPanel>
+        <MySearchForm />
+        {/* ... */}
+      </PopoverPanel>
+    </Popover>
   );
 }
