4a57264547dc0d74798c196aab5c4e3d95e60d46
Changnam Go <changnam.go@gmail.com>
2025-03-03 16:43:09 +0900

    headlessui dialog 에 transition 적용

--- app/dialog/page.tsx
+++ app/dialog/page.tsx
@@ -2,17 +2,11 @@
 import {
   Description,
   Dialog,
-  DialogBackdrop,
   DialogPanel,
   DialogTitle,
-  useClose,
 } from "@headlessui/react";
 import { useState } from "react";
 
-function MyButton() {
-  let close = useClose();
-  return <button onClick={() => close()}>Close dialog</button>;
-}
 export default function Example() {
   let [isOpen, setIsOpen] = useState(false);
 
@@ -22,28 +16,23 @@ export default function Example() {
       <Dialog
         open={isOpen}
         onClose={() => setIsOpen(false)}
-        className="relative z-50"
+        transition
+        className="fixed inset-0 flex w-screen items-center justify-center bg-black/30 p-4 transition duration-300 ease-out data-[closed]:opacity-0"
       >
-        {/* The backdrop, rendered as a fixed sibling to the panel container */}
-        <DialogBackdrop className="fixed inset-0 bg-black/30" />
-
-        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-          <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-            <DialogTitle className="font-bold">Deactivate account</DialogTitle>
-            <Description>
-              This will permanently deactivate your account
-            </Description>
-            <p>
-              Are you sure you want to deactivate your account? All of your data
-              will be permanently removed.
-            </p>
-            <div className="flex gap-4">
-              <button onClick={() => setIsOpen(false)}>Cancel</button>
-              <button onClick={() => setIsOpen(false)}>Deactivate</button>
-            </div>
-            <MyButton />
-          </DialogPanel>
-        </div>
+        <DialogPanel className="max-w-lg space-y-4 bg-white p-12">
+          <DialogTitle className="font-bold">Deactivate account</DialogTitle>
+          <Description>
+            This will permanently deactivate your account
+          </Description>
+          <p>
+            Are you sure you want to deactivate your account? All of your data
+            will be permanently removed.
+          </p>
+          <div className="flex gap-4">
+            <button onClick={() => setIsOpen(false)}>Cancel</button>
+            <button onClick={() => setIsOpen(false)}>Deactivate</button>
+          </div>
+        </DialogPanel>
       </Dialog>
     </>
   );
