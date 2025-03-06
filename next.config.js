70af20b3ff9597660c01657c6e598f865b66cceb
Changnam Go <changnam.go@gmail.com>
2025-03-06 11:43:44 +0900

    가짜 데이터 생성을 위한 package 설치 및 util 함수들 작성

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ module.exports = {
         hostname: "cdn.shopify.com",
         pathname: "/s/files/**",
       },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
+      },
     ],
   },
 };
