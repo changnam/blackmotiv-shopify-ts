da08c190725d125e1ac41b5ac183811f1844df15
Changnam Go <changnam.go@gmail.com>
2025-03-08 19:16:20 +0900

    useState 사용시 lazy initial state 적용안했을때, 초기화 함수는 mount 시에만 사용되어 지지만, rerender 시마다 호출되어 계산됨, 함수 방식으로 해야 mount 때만 호출됨

---
+++ app/lazyinit/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
+function untangle(){
+  console.log("untangle called");
+  return 1;
+}
+export default function LazyInitPage(){
+  const [shiny, setShiny] = useState(untangle());
+
+  async function increment(){
+    const delayPromise = new Promise((resolve,reject) => setTimeout(() => resolve("blabla"),5000));
+    await delayPromise.then(() => console.log("resolved"));
+
+    setShiny((prev) => prev + 1);
+  }
+  return (
+    <div>
+      <p>{shiny}</p>
+      <button onClick={() => increment()}>increment</button>
+    </div>
+  );
+}
