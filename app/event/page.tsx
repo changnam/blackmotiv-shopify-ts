c976fd4bfe91236fcdd4e4e1fb1e372e679083fe
Changnam Go <changnam.go@gmail.com>
2025-03-09 18:15:23 +0900

    event bubbling , button 의 click event 가 div 로 전파확인, target 은 동일하나 currentTarget 은 달라짐

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -1,13 +1,15 @@
 import { DispatchEvent } from "@/components/events/dispatch-event";
+import { EventBubbling } from "@/components/events/event-bubbling";
 import { OnClick } from "@/components/events/on-click";
 import { ReactOnClick } from "@/components/events/react-on-click";
 
 export default function EventPage() {
   return (
     <div id="root">
-      <OnClick />
+      {/* <OnClick />
       <ReactOnClick />
-      <DispatchEvent />
+      <DispatchEvent /> */}
+      <EventBubbling/>
     </div>
   );
 }
